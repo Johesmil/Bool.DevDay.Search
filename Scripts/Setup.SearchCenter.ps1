4bd65a8f557dfd089f0dd5a7288ee6998d603d35
EmelieSchlyter <schlyter.emelie@gmail.com>
2015-06-04 11:25:08 +0200

    Lista ranking models

--- Scripts/Setup.SearchCenter.ps1
+++ Scripts/Setup.SearchCenter.ps1
@@ -1,3 +1,9 @@
 Import-Module .\Module.Main.psm1
 
+function ListRankings()
+{
+	$ssa = Get-SPEnterpriseSearchServiceApplication -Identity "Search Service Application";
+	$owner = Get-SPenterpriseSearchOwner -Level ssa;
+	$list = Get-SPEnterpriseSearchRankingModel -SearchApplication $ssa -Owner $owner;
+}
 
